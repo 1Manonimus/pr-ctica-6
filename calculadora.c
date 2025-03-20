959180a48a884a25be4e2ab56d5b77a31e85c220
Elver Galarga <mangoancestral@gmail.com>
2025-03-20 14:46:20 -0600

    Create calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,26 @@
+int main()
+{
+    int sum=0;
+    int a=0;
+    int b=0;
+    int n;
+ 
+    printf ("escoje una opción;  1- suma  2-resta  3-multiplicación  4- división");
+ 
+ scanf ("%varaible", n);
+ {
+ if (n==0);
+ printf ("Ingrese los números;");
+ {
+ scanf ("%variable", a);
+ 
+ scanf ("%variable",b);
+ }
+ {
+ else
+ }
+ if ()
+ }
+
+    return 0;
+}
